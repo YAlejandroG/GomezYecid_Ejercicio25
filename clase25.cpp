31c53da8dfa8c6a7bd27972de4134b02a89fb2cf
Default user <yalejandro.gomez@gmail.com>
2019-11-06 21:41:28 +0000

    Ejercicio completo

--- clase25.cpp
+++ clase25.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 
-int relleno(int f,int c);
-int M[0][0];
+int relleno(int num,float **M);
+int imprimeCabezas(int num,float **M);
+int imprimir(int num,float **M);
 
 int main(){
     int num;
@@ -11,19 +12,51 @@ int main(){
         std::cout<<"Número fuera de rango, ingresar nuevamente"<<std::endl;
         std::cin>>num;
     }
-    M[num+2][num];
-    relleno(num+2,num);
+    float **M = new float *[num+2];
+    for (int i=0;i<num+2;++i){
+        M[i] =new float[num];
+    }
+    relleno(num,M);
+    imprimir(num,M);
+    imprimeCabezas(num,M);
     
     return 0;
 }
 
-int relleno(int f,int c){
-    for(int i=0;i<f;i++){
-        for(int j=0;j<c;j++){
+int relleno(int num,float **M){
+    for(int i=0;i<num+2;i++){
+        for(int j=0;j<num;j++){
             M[i][j] = i+j;
-            std::cout<<M[i][j]<<std::endl;
         }
     }
     return 0;
 }
 
+int imprimeCabezas(int num,float **M){
+    float col=0;
+    std::cout<<"Primera fila:"<<std::endl;
+    for(int i=0;i<num;i++){
+        std::cout<<M[0][i]<<"\t";
+    }
+    std::cout<<std::endl;
+    for(int j=0;j<num+2;j++){
+        col += M[j][0];
+    }
+    std::cout<<"Suma elementos primera columna:"<<std::endl;
+    std::cout<<col<<std::endl;
+    
+    return 0;
+}
+
+
+
+int imprimir(int num,float **M){
+    std::cout<<"Matriz:"<<std::endl;
+    for (int i=0;i<num+2;i++){
+        for (int j=0;j<num;j++) {
+            std::cout<<M[i][j]<<"\t";
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
+}
